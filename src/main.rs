ebf65f3f94d85b78cefad0c8891f2a333056016a
hanishkvc <hanishkvc@gmail.com>
2022-11-11 00:17:11 +0530

    Main: Create a basic sdl application loop
    Clear the window.
    
    Fetch any pending events.
    
    [TODO] Change/Draw the content to display.
    
    Present the updated content to user/screen.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 //! HanishKVC, 2022
 //!
 
-use sdl2::{self, VideoSubsystem, Sdl, video::Window, EventPump, render::WindowCanvas};
+use sdl2::{self, VideoSubsystem, Sdl, EventPump, render::WindowCanvas, pixels};
 
 fn sdl_init() -> (Sdl, VideoSubsystem, WindowCanvas, EventPump) {
     let sctxt = sdl2::init().unwrap();
@@ -14,8 +14,35 @@ fn sdl_init() -> (Sdl, VideoSubsystem, WindowCanvas, EventPump) {
     return (sctxt, sv, swc, se);
 }
 
+
 fn main() {
     println!("Hello, world!");
-    let (sctxt, sv, swc, se) = sdl_init();
-    
+    let (_sctxt, _sv, mut swc, mut se) = sdl_init();
+    let mut dcolor = 20;
+
+    'mainloop: loop {
+        swc.set_draw_color(pixels::Color::RGB(20+dcolor, 200, 20));
+        swc.clear();
+        for ev in se.poll_iter() {
+            use sdl2::event::Event;
+            use sdl2::keyboard::Keycode;
+            match ev {
+                Event::Quit { timestamp: _ } => break 'mainloop,
+                Event::KeyDown { timestamp: _, window_id: _, keycode, scancode: _, keymod: _, repeat: _ } => {
+                    match keycode.unwrap() {
+                        Keycode::W => {
+                            dcolor += 20;
+                        }
+                        _ => {
+
+                        }
+                    }
+                },
+                _ => (),
+            }
+        }
+        swc.present();
+        std::thread::sleep(std::time::Duration::from_millis(40));
+    }
+
 }
