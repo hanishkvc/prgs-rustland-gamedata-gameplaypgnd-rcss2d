462fd5db106e57922fb60af62f71a957a66c596d
hanishkvc <hanishkvc@gmail.com>
2022-11-10 23:55:11 +0530

    Initialise SDL and get a Window Canvas/Renderer & Event iterator

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,19 @@
 //! HanishKVC, 2022
 //!
 
+use sdl2::{self, VideoSubsystem, Sdl, video::Window, EventPump, render::WindowCanvas};
+
+fn sdl_init() -> (Sdl, VideoSubsystem, WindowCanvas, EventPump) {
+    let sctxt = sdl2::init().unwrap();
+    let sv = sctxt.video().unwrap();
+    let sw = sv.window("Playback", 800, 600).build().unwrap();
+    let swc = sw.into_canvas().build().unwrap();
+    let se = sctxt.event_pump().unwrap();
+    return (sctxt, sv, swc, se);
+}
 
 fn main() {
     println!("Hello, world!");
+    let (sctxt, sv, swc, se) = sdl_init();
+    
 }
