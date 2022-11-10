c8830c0bef0ce9057f77143e98c92e88539e5adb
hanishkvc <hanishkvc@gmail.com>
2022-11-11 00:58:38 +0530

    Entities: Update entities, More Entities spread out in field

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,19 @@ fn sdl_init() -> (Sdl, VideoSubsystem, WindowCanvas, EventPump) {
     return (sctxt, sv, swc, se);
 }
 
-fn setup_entities(nplayers: usize) -> Vec<Entity> {
+fn setup_entities(nplayers: i32) -> Vec<Entity> {
     let mut vplayers = Vec::new();
-    for _i in 0..nplayers {
-        vplayers.push(Entity::new((0,0), Color::RGB(200, 0, 0)));
+    for i in 0..nplayers {
+        vplayers.push(Entity::new((i*20i32,0), Color::RGB(200, 0, 0)));
     }
     return vplayers;
+}
+
+fn update_entities(vplayers: &mut Vec<Entity>, bpos: i32) {
+    vplayers[0].pos_set(bpos, bpos);
+    for i in 1..vplayers.len() {
+        vplayers[i].pos_update(1, 1);
+    }
 }
 
 
@@ -32,11 +39,14 @@ fn main() {
     println!("Hello, world!");
     let (_sctxt, _sv, mut swc, mut se) = sdl_init();
     let mut dcolor = 20;
-    let mut players = setup_entities(2);
+    let mut players = setup_entities(4);
 
     'mainloop: loop {
+        // Clear the background
         swc.set_draw_color(Color::RGB(20+dcolor, 200, 20));
         swc.clear();
+
+        // handle any pending events
         for ev in se.poll_iter() {
             use sdl2::event::Event;
             use sdl2::keyboard::Keycode;
@@ -55,9 +65,11 @@ fn main() {
                 _ => (),
             }
         }
-        players[0].pos_set(dcolor as i32, dcolor as i32);
-        players[1].pos_update(1, 1);
 
+        // Update the entities
+        update_entities(&mut players, dcolor as i32);
+
+        // Draw entities
         for i in 0..players.len() {
             players[i].draw(&mut swc);
         }
