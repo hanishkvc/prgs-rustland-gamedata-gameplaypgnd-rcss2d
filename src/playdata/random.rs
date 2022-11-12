e1cb7f0801dc35e013dd42e6cae164a48ff0d845
hanishkvc <hanishkvc@gmail.com>
2022-11-12 17:46:44 +0530

    RandomPlayData: Get UpdateDummy behavior wrt NextRecord (NonInBtw)

--- src/playdata/random.rs
+++ src/playdata/random.rs
@@ -20,22 +20,35 @@ pub struct RandomData {
     bcnt: usize,
     apos: Vec<(f32,f32)>,
     bpos: Vec<(f32,f32)>,
+    amov: Vec<(f32, f32)>,
+    bmov: Vec<(f32, f32)>,
+    achg: Vec<usize>,
+    bchg: Vec<usize>,
+    rcnt: usize,
 }
 
 impl RandomData {
 
     pub fn new(spr: f32, acnt: usize, bcnt: usize) -> RandomData {
         let mut apos = Vec::new();
+        let mut amov = Vec::new();
+        let mut achg = Vec::new();
         for _i in 0..acnt {
             let fx = (rand::random::<u32>() % entities::SCREEN_WIDTH) as f32;
             let fy = (rand::random::<u32>() % entities::SCREEN_HEIGHT) as f32;
             apos.push((fx, fy));
+            amov.push((0.0, 0.0));
+            achg.push(rand::random::<usize>() % 128);
         }
         let mut bpos = Vec::new();
+        let mut bmov = Vec::new();
+        let mut bchg = Vec::new();
         for _i in 0..bcnt {
             let fx = (rand::random::<u32>() % 400) as f32;
             let fy = (rand::random::<u32>() % 400) as f32;
             bpos.push((fx, fy));
+            bmov.push((0.0, 0.0));
+            bchg.push(rand::random::<usize>() % 128);
         }
 
         RandomData {
@@ -46,6 +59,11 @@ impl RandomData {
             bcnt: bcnt,
             apos: apos,
             bpos: bpos,
+            amov: amov,
+            bmov: bmov,
+            rcnt: 0,
+            achg: achg,
+            bchg: bchg,
         }
     }
 
@@ -67,6 +85,7 @@ impl PlayData for RandomData {
         return false;
     }
 
+    #[cfg(feature="inbetween_frames")]
     fn next_record(&mut self) -> PositionsUpdate {
         let mut pu = PositionsUpdate::new();
         for i in 0..self.acnt {
@@ -88,6 +107,33 @@ impl PlayData for RandomData {
             }
             self.bpos[i].0 += dx as f32;
             self.bpos[i].1 += dy as f32;
+            pu.bteampositions.push((i as i32, self.bpos[i].0, self.bpos[i].1));
+        }
+        pu
+    }
+
+    #[cfg(not(feature="inbetween_frames"))]
+    fn next_record(&mut self) -> PositionsUpdate {
+        self.rcnt += 1;
+        let mut pu = PositionsUpdate::new();
+        for i in 0..self.acnt {
+            if self.rcnt % self.achg[i] == 0 {
+                let dx = (rand::random::<i32>() % 4) as f32;
+                let dy = (rand::random::<i32>() % 4) as f32;
+                self.amov[i] = (dx, dy);
+            }
+            self.apos[i].0 += self.amov[i].0;
+            self.apos[i].1 += self.amov[i].1;
+            pu.ateampositions.push((i as i32, self.apos[i].0, self.apos[i].1));
+        }
+        for i in 0..self.bcnt {
+            if self.rcnt % self.bchg[i] == 0 {
+                let dx = (rand::random::<i32>() % 4) as f32;
+                let dy = (rand::random::<i32>() % 4) as f32;
+                self.bmov[i] = (dx, dy);
+            }
+            self.bpos[i].0 += self.bmov[i].0;
+            self.bpos[i].1 += self.bmov[i].1;
             pu.bteampositions.push((i as i32, self.bpos[i].0, self.bpos[i].1));
         }
         pu
