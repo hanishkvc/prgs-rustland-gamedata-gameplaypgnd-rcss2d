d306554714b7f897d2fbe33c009e689083833fd8
hanishkvc <hanishkvc@gmail.com>
2022-11-12 16:07:16 +0530

    PlayData: Random type playdata generator added

---
+++ src/playdata/random.rs
@@ -0,0 +1,68 @@
+//!
+//! Random play data
+//! HanishKVC, 2022
+//!
+
+use rand;
+
+use super::PlayData;
+use super::PositionsUpdate;
+
+pub struct RandomData {
+    /// seconds per record
+    spr: f32,
+    /// frames per record
+    fpr: f32,
+    next: f32,
+    acnt: usize,
+    bcnt: usize,
+}
+
+impl RandomData {
+
+    pub fn new(spr: f32, acnt: usize, bcnt: usize) -> RandomData {
+        RandomData {
+            spr: spr,
+            fpr: 0.0,
+            next: 0.0,
+            acnt: acnt,
+            bcnt: bcnt,
+        }
+    }
+
+}
+
+impl PlayData for RandomData {
+
+    fn setup(&mut self, fps: f32) {
+        self.fpr = fps*self.spr;
+        self.next = 0.0;
+    }
+
+    fn next_frame_is_record_ready(&mut self) -> bool {
+        self.next += 1.0;
+        if self.next >= self.fpr {
+            self.next = self.next - self.fpr;
+            return true;
+        }
+        return false;
+    }
+
+    fn next_record(&mut self) -> PositionsUpdate {
+        let mut pu = PositionsUpdate::new();
+        for i in 0..self.acnt {
+            let dx = (rand::random::<i32>() % 128) as f32;
+            let dy = (rand::random::<i32>() % 128) as f32;
+            pu.ateampositions.push((i as i32, dx, dy));
+        }
+        let maxx = rand::random::<i32>() % 256;
+        for i in 0..self.bcnt {
+            let maxy = rand::random::<i32>() % 256;
+            let dx = (rand::random::<i32>() % maxx) as f32;
+            let dy = (rand::random::<i32>() % maxy) as f32;
+            pu.bteampositions.push((i as i32, dx, dy));
+        }
+        pu
+    }
+
+}
