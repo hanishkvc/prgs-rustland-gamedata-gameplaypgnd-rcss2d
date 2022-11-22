2aa140d27a4e86c736beab9c0929e313c1ec6977
hanishkvc <hanishkvc@gmail.com>
2022-11-22 10:16:10 +0530

    Passes: KickData struct, Initial skel infering passes from kicks

--- src/proc/passes.rs
+++ src/proc/passes.rs
@@ -3,6 +3,11 @@
 //! HanishKVC, 2022
 //!
 
+
+const SCORE_BAD_PASS: f32 = -0.5;
+const SCORE_GOOD_PASS: f32 = 1.0;
+const SCORE_SELF_PASS: f32 = 0.1;
+
 struct Players {
     aplayers: Vec<(usize, f32)>,
     bplayers: Vec<(usize, f32)>,
@@ -11,7 +16,7 @@ struct Players {
 impl Players {
 
     fn new(acnt: usize, bcnt: usize) -> Players {
-        let mut players = Players {
+        let mut     players = Players {
             aplayers: Vec::new(),
             bplayers: Vec::new(),
         };
@@ -26,10 +31,55 @@ impl Players {
 
 }
 
-/// (time, (side, playerid), (nx, ny))
-type KickData = (u32, (char, usize), (f32, f32));
+struct KickData {
+    time: usize,
+    side: char,
+    playerid: usize,
+    pos: (f32, f32),
+}
+
+impl KickData {
+
+    fn new(time: usize, side: char, playerid: usize, pos: (f32,f32)) -> KickData {
+        KickData {
+            time: time,
+            side: side,
+            playerid: playerid,
+            pos: pos,
+        }
+    }
+
+}
 
 pub struct Passes {
     players: Players,
     kicks: Vec<KickData>,
+}
+
+impl Passes {
+
+    fn new(acnt: usize, bcnt: usize) -> Passes {
+        Passes {
+            players: Players::new(acnt, bcnt),
+            kicks: Vec::new(),
+        }
+    }
+
+    fn add_kick(&mut self, kick: KickData) {
+        let ik = self.kicks.len();
+        if ik > 0 {
+            let prev = &self.kicks[ik];
+            if prev.side != kick.side {
+                self.players.aplayers[prev.playerid].1 += SCORE_BAD_PASS;
+            } else {
+                if prev.playerid == kick.playerid {
+                    self.players.aplayers[prev.playerid].1 += SCORE_SELF_PASS;
+                } else {
+                    self.players.aplayers[prev.playerid].1 += SCORE_GOOD_PASS;
+                }
+            }
+        }
+        self.kicks.push(kick);
+    }
+
 }
