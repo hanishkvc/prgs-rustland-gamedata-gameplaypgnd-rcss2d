b51327eae1c45e875b4ded6f0d54b8f2524bd12c
hanishkvc <hanishkvc@gmail.com>
2022-11-24 19:12:06 +0530

    SimObjects:SimBall: Initial go at next_record with interpolation

--- src/entities/simobjs.rs
+++ src/entities/simobjs.rs
@@ -3,10 +3,13 @@
 //! HanishKVC, 2022
 //!
 
+/// A interpolated ball
 struct SimBall {
     vdata: Vec<String>,
+    vin: usize,
     ltime: usize,
     lpos: (f32, f32),
+    cpos: (f32, f32),
     mov: (f32, f32),
 }
 
@@ -21,15 +24,36 @@ impl SimBall {
         }
         SimBall {
             vdata: vdata,
+            vin: 0,
             ltime: 0,
-            lpos: (0.0,0.0),
-            mov: (0.0,0.0),
+            lpos: (0.0, 0.0),
+            cpos: (0.0, 0.0),
+            mov: (0.0, 0.0),
         }
     }
 
     pub fn next_record(&mut self, ctime: usize) -> (f32, f32) {
-        //if ctime < self.
-        (0.0, 0.0)
+        while ctime > self.ltime {
+            let sdata = &self.vdata[self.vin];
+            self.vin += 1;
+            let sdata = sdata.split(',').collect::<Vec<&str>>();
+            self.ltime = sdata[0].parse().unwrap();
+            let fx = sdata[1].parse().unwrap();
+            let fy = sdata[2].parse().unwrap();
+            self.lpos = (fx, fy);
+            let dt = self.ltime as isize - ctime as isize;
+            if dt < 0 {
+                continue;
+            } else if dt == 0 {
+                self.cpos = self.lpos;
+                return self.lpos;
+            }
+            let dx = (self.lpos.0 - self.cpos.0)/(dt as f32 +1.0);
+            let dy = (self.lpos.1 - self.cpos.1)/(dt as f32 +1.0);
+            self.mov = (dx,dy);
+        }
+        self.cpos = (self.cpos.0 + self.mov.0, self.cpos.1 + self.mov.1);
+        self.cpos
     }
 
 }
