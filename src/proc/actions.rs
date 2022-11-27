d1547c29cd572638636381b2d8cb3c7ef5ebaf84
hanishkvc <hanishkvc@gmail.com>
2022-11-27 12:06:20 +0530

    ProcActions: Allow ball position to be included in ActionData
    Just for reference, will revert

--- src/proc/actions.rs
+++ src/proc/actions.rs
@@ -241,17 +241,19 @@ pub struct ActionData {
     playerid: usize,
     #[allow(dead_code)]
     pub pos: (f32, f32),
+    ballpos: (f32, f32),
     action: AIAction,
 }
 
 impl ActionData {
 
-    pub fn new(time: usize, side: char, playerid: usize, pos: (f32,f32), action: AIAction) -> ActionData {
+    pub fn new(time: usize, side: char, playerid: usize, pos: (f32,f32), ballpos: (f32,f32), action: AIAction) -> ActionData {
         ActionData {
             time: time,
             side: side,
             playerid: playerid,
             pos: pos,
+            ballpos: ballpos,
             action: action,
         }
     }
