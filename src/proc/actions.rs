2ea51ae3a0d2b905bbd8b52f0def5ec371f916e2
hanishkvc <hanishkvc@gmail.com>
2022-11-27 16:39:10 +0530

    ProcActions: Make handle_catch more full, including same side seq

--- src/proc/actions.rs
+++ src/proc/actions.rs
@@ -226,7 +226,7 @@ impl AIAction {
             AIAction::None => (0.0, 0.0,0.0, 0.0,0.0),
             AIAction::Kick => (0.6, 0.5,0.5, -0.8,0.8),
             AIAction::Tackle => (0.4, 0.0,0.0, -0.5,0.5),
-            AIAction::Catch => (1.0, 0.0,0.0, 0.2,0.8),
+            AIAction::Catch => (1.0, 0.4,0.4, 0.2,0.8), // Give some score to otherprev player bcas they tried to achieve a goal
             AIAction::Goal => (1.0, 1.0,0.0, -1.0,0.0),
         }
     }
@@ -663,12 +663,17 @@ impl ActionsInfo {
                 panic!("DBUG:{}:HandleAction:Catch:None{:?}->Catch{:?} shouldnt occur", MTAG, prevactd, curactd);
             },
             AIAction::Kick | AIAction::Tackle => {
-                if prevactd.side != curactd.side {
-                    let ppscore = score.0 * score.3;
-                    self.players.score(prevactd.side, prevactd.playerid, ppscore);
-                    let cpscore = score.0 * score.4;
-                    self.players.score(curactd.side, curactd.playerid, cpscore);
+                let ppscore;
+                let cpscore;
+                if prevactd.side == curactd.side {
+                    ppscore = score.0 * score.1;
+                    cpscore = score.0 * score.2;
+                } else {
+                    ppscore = score.0 * score.3;
+                    cpscore = score.0 * score.4;
                 }
+                self.players.score(prevactd.side, prevactd.playerid, ppscore);
+                self.players.score(curactd.side, curactd.playerid, cpscore);
                 return HAReturn::Done(true);
             },
             AIAction::Catch | AIAction::Goal => {
