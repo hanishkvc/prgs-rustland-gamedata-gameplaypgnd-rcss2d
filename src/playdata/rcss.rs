483d5c100f1dd4428bd5decf4a97d92f15788bd1
hanishkvc <hanishkvc@gmail.com>
2022-11-30 15:02:35 +0530

    RCSS: Check what is Ball2Player and Player2Ball

--- src/playdata/rcss.rs
+++ src/playdata/rcss.rs
@@ -15,6 +15,8 @@ pub const STATE_KICK: u32           = 0x00002;
 pub const STATE_KICK_FAULT: u32     = 0x00004;
 pub const STATE_CATCH: u32          = 0x00010;
 pub const STATE_CATCH_FAULT: u32    = 0x00020;
+pub const STATE_BALL2PLAYER: u32    = 0x00040;
+pub const STATE_PLAYER2BALL: u32    = 0x00080;
 pub const STATE_TACKLE: u32         = 0x01000;
 pub const STATE_TACKLE_FAULT: u32   = 0x02000;
 pub const STATE_REDCARD: u32        = 0x80000;
@@ -29,18 +31,34 @@ pub fn handle_state(state: u32) -> (Action, Card) {
     } else if state & STATE_YELLOWCARD == STATE_YELLOWCARD {
         card = Card::Yellow;
     }
+    let mut statecnt = 0;
     if state & STATE_KICK == STATE_KICK {
         action = Action::Kick(true);
+        statecnt += 1;
     } else if state & STATE_KICK_FAULT == STATE_KICK_FAULT {
         action = Action::Kick(false);
+        statecnt += 1;
     } else if state & STATE_CATCH == STATE_CATCH {
         action = Action::Catch(true);
+        statecnt += 1;
     } else if state & STATE_CATCH_FAULT == STATE_CATCH_FAULT {
         action = Action::Catch(false);
+        statecnt += 1;
     } else if state & STATE_TACKLE == STATE_TACKLE {
         action = Action::Tackle(true);
+        statecnt += 1;
     } else if state & STATE_TACKLE_FAULT == STATE_TACKLE_FAULT {
         action = Action::Tackle(false);
+        statecnt += 1;
+    } else if state & STATE_BALL2PLAYER == STATE_BALL2PLAYER {
+        statecnt += 1;
+        eprintln!("DBUG:RCSS:Ball2Player");
+    } else if state & STATE_PLAYER2BALL == STATE_PLAYER2BALL {
+        statecnt += 1;
+        eprintln!("DBUG:RCSS:Player2Ball");
+    }
+    if statecnt > 1 {
+        panic!("DBUG:RCSS:MultipleStates:{}:{:x}", statecnt, state);
     }
     return (action, card);
 }
